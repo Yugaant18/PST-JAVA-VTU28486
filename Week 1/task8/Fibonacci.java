0028a32f587efa508119451cd4c7e37c6e95c800
Yugaant A <vtu28486@veltech.edu.in>
2026-07-25 10:13:27 +0530

    Add files via upload

---
+++ Week 1/task8/Fibonacci.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+
+        int a = 0;
+        int b = 1;
+
+        if (n == 1) {
+            System.out.println(a);
+        }
+        else if (n == 2) {
+            System.out.println(b);
+        }
+        else {
+            for (int i = 3; i <= n; i++) {
+                int c = a + b;
+                a = b;
+                b = c;
+            }
+
+            System.out.println("Nth Fibonacci = " + b);
+        }
+    }
+}
