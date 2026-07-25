245aaa62355602822c3f967f3c04122027915b20
Yugaant A <vtu28486@veltech.edu.in>
2026-07-25 10:10:15 +0530

    Add files via upload

---
+++ Week 1/task7/DigitSum.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class DigitSum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        System.out.println("1. Sum of Even Digits");
+        System.out.println("2. Sum of Odd Digits");
+        System.out.print("Enter your choice: ");
+        int choice = sc.nextInt();
+
+        int sum = 0;
+
+        while (num > 0) {
+            int digit = num % 10;
+
+            if (choice == 1 && digit % 2 == 0) {
+                sum += digit;
+            }
+            else if (choice == 2 && digit % 2 != 0) {
+                sum += digit;
+            }
+
+            num = num / 10;
+        }
+
+        System.out.println("Sum = " + sum);
+    }
+}
